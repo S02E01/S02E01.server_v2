9e2a5731a4910ccfc3f18392041b9a265d27af97
I0HuKc <web-disain@mail.ua>
2021-10-21 19:22:25 +0300

    Изменил типо поля create_at

--- migrations/2021-10-20-160504_users_table/up.sql
+++ migrations/2021-10-20-160504_users_table/up.sql
@@ -2,5 +2,5 @@ CREATE TABLE users (
     id BIGSERIAL NOT NULL,
     chat_id BIGINT NOT NULL PRIMARY KEY,
     user_role INT NOT NULL,
-    create_at TIMESTAMP WITH TIME ZONE
+    create_at TEXT NOT NULL
 );
