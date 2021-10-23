2924593324c5af7952f3b618a2a417ab98ce1d54
I0HuKc <web-disain@mail.ua>
2021-10-23 22:30:27 +0300

    Доработал таблицу пользователей

--- migrations/2021-10-20-160504_users_table/up.sql
+++ migrations/2021-10-20-160504_users_table/up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
-    id BIGSERIAL NOT NULL,
     chat_id BIGINT NOT NULL PRIMARY KEY,
     user_role INT NOT NULL,
-    create_at TEXT NOT NULL
+    created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    updated_at TIMESTAMP
 );
