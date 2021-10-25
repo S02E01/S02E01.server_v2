5139139109f85e3e4309d4074cee2708e34c5dc0
I0HuKc <web-disain@mail.ua>
2021-10-25 14:32:43 +0300

    Обновил схему таблицы

--- migrations/2021-10-20-160504_users_table/up.sql
+++ migrations/2021-10-20-160504_users_table/up.sql
@@ -1,6 +1,7 @@
 CREATE TABLE users (
     chat_id BIGINT NOT NULL PRIMARY KEY,
-    user_role INT NOT NULL,
+    hash TEXT NOT NULL,
+    role INT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
     updated_at TIMESTAMP
 );
